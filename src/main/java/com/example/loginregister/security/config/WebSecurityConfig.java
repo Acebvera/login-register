4e76bbee2d4296fd59f0c02edf49e98d275a4203
alex <alex@gmail.com>
2022-12-20 17:20:19 +0100

    Bajada versión springboot de 3.0.0 -> 2.7.6 y corregidos errores con los imports en AppUser.java

--- src/main/java/com/example/loginregister/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/loginregister/security/config/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package com.example.loginregister.security.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 import com.example.loginregister.appuser.AppUserService;
 
@@ -16,13 +17,8 @@ import lombok.AllArgsConstructor;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	private final AppUserService appUserService = new AppUserService();
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests().requestMatchers("/api/v*/registration").permitAll().anyRequest().authenticated().and().formLogin();
-		return null;
-	}
 	
 }
