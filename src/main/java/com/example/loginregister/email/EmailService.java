2f31056a5e63379670c3f513a351a78e0b7ba280
alex <alex@gmail.com>
2023-01-08 17:43:00 +0100

    Corregido envio de mensaje en EmailService.java. Añadidas queries en ConfirmationTokenRepository.java y AppUserRepository.java. Añadida configuración del correo en el archivo application.yml

--- src/main/java/com/example/loginregister/email/EmailService.java
+++ src/main/java/com/example/loginregister/email/EmailService.java
@@ -32,6 +32,7 @@ public class EmailService implements EmailSender{
 			helper.setTo(to);
 			helper.setSubject("Confirm your email.");
 			helper.setFrom("myMail@mail.com");
+			mailSender.send(mimeMessage);
 		} catch (MessagingException e) {
 			LOGGER.error("Failed to send email", e);
 			throw new IllegalStateException("Failed to send email.");
